587dacfb6399796d1d90903f75b37c7b6742944e
217heidai <1010472380@qq.com>
2018-12-13 17:36:03 +0800

    Add files via upload

---
+++ openssl_build_armeabi-v7a.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+#openssl build for android: armeabi, armeabi-v7a, x86, x86_64, arm64-v8a, mips, mips64
+
+ANDROID_TARGET_API=28
+ANDROID_TARGET_ABI=armeabi-v7a
+GCC_VERSION=4.9
+OUTPUT_PATH=$HOME/work/SSL/Android/openssl-android/armeabi-v7a
+
+rm -rf ${OUTPUT_PATH}
+
+./openssl_build.sh ${ANDROID_TARGET_API} ${ANDROID_TARGET_ABI} ${GCC_VERSION} ${OUTPUT_PATH}
+ 
+rm -rf ${OUTPUT_PATH}/bin
+rm -rf ${OUTPUT_PATH}/certs
+rm -rf ${OUTPUT_PATH}/lib/engines
+rm -rf ${OUTPUT_PATH}/lib/pkgconfig
+rm -rf ${OUTPUT_PATH}/man
+rm -rf ${OUTPUT_PATH}/misc
+rm -rf ${OUTPUT_PATH}/private
+rm -f ${OUTPUT_PATH}/openssl.cnf
+
