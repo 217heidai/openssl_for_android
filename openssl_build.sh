4f64f82ecdfa021ce2d36c80609337037a63d4cc
jianxu1@chinaums.com <jianxu1@chinaums.com>
2021-10-12 14:19:40 +0800

    3.0.0

--- openssl_build.sh
+++ openssl_build.sh
@@ -43,7 +43,7 @@ then
 elif [ "$ANDROID_TARGET_ABI" == "arm64-v8a" ]
 then
     export ANDROID_NDK_ROOT=${ANDROID_NDK_PATH}
-    PATH=$ANDROID_NDK_ROOT/toolchains/aarch64-linux-android-4.9/prebuilt/linux-x86_64/bin:$PATH
+    PATH=$ANDROID_NDK_ROOT/toolchains/llvm/prebuilt/linux-x86_64/bin:$ANDROID_NDK_ROOT/toolchains/arm-linux-androideabi-4.9/prebuilt/linux-x86_64/bin:$ANDROID_NDK_ROOT/toolchains/aarch64-linux-android-4.9/prebuilt/linux-x86_64/bin:$PATH
     cd ${OPENSSL_TMP_FOLDER}
     ./Configure android-arm64 -D__ANDROID_API__=${ANDROID_TARGET_API} -static no-shared no-tests --prefix=${OUTPUT_PATH}
     build_library
