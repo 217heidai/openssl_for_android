bf9fb5bfd41d0b474d5a2ff4bb9183970fefbd63
jianxu1@chinaums.com <jianxu1@chinaums.com>
2021-10-12 13:34:19 +0800

    3.0.0

--- openssl_build.sh
+++ openssl_build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -e
 
 WORK_PATH=$(cd "$(dirname "$0")";pwd)
-ANDROID_NDK_PATH=${WORK_PATH}/android-ndk-r14b
+ANDROID_NDK_PATH=${WORK_PATH}/android-ndk-r23
 OPENSSL_SOURCES_PATH=${WORK_PATH}/openssl-3.0.0
 ANDROID_TARGET_API=$1
 ANDROID_TARGET_ABI=$2
@@ -27,7 +27,7 @@ function build_library {
 if [ "$ANDROID_TARGET_ABI" == "armeabi" ]
 then
     export ANDROID_NDK_ROOT=${ANDROID_NDK_PATH}
-    PATH=$ANDROID_NDK_ROOT/toolchains/arm-linux-androideabi-4.9/prebuilt/linux-x86_64/bin:$PATH
+    PATH=$ANDROID_NDK_ROOT/toolchains/llvm/prebuilt/linux-x86_64/bin:$ANDROID_NDK_ROOT/toolchains/arm-linux-androideabi-4.9/prebuilt/linux-x86_64/bin:$PATH
     cd ${OPENSSL_TMP_FOLDER}
     ./Configure android-arm -D__ANDROID_API__=${ANDROID_TARGET_API} no-shared no-tests --prefix=${OUTPUT_PATH}
     build_library
@@ -35,7 +35,7 @@ then
 elif [ "$ANDROID_TARGET_ABI" == "armeabi-v7a" ]
 then
     export ANDROID_NDK_ROOT=${ANDROID_NDK_PATH}
-    PATH=$ANDROID_NDK_ROOT/toolchains/arm-linux-androideabi-4.9/prebuilt/linux-x86_64/bin:$PATH
+    PATH=$ANDROID_NDK_ROOT/toolchains/llvm/prebuilt/linux-x86_64/bin:$ANDROID_NDK_ROOT/toolchains/arm-linux-androideabi-4.9/prebuilt/linux-x86_64/bin:$PATH
     cd ${OPENSSL_TMP_FOLDER}
     ./Configure android-arm -D__ANDROID_API__=${ANDROID_TARGET_API} no-shared no-tests --prefix=${OUTPUT_PATH}
     build_library
